2ebe57416a5fd162984cb21429af7bbdb103e8ae
bryan-lee-git <bryanvinyl@gmail.com>
2018-12-29 21:09:48 -0700

    fixed merge conflicts

--- yoso/client/src/App.js
+++ yoso/client/src/App.js
@@ -21,23 +21,12 @@ class App extends Component {
             </header>
 
             <main>
-<<<<<<< HEAD
-              <div className="container">
-                <Route exact path="/" component={Login} />
-                <Route exact path="/MyLists" component={MyLists} />
-                <Route exact path="/Pantry" component={Pantry} />
-                {/* personal list will need a param in order for it to go to the right list */}
-                <Route exact path="/PersonalList" component={PersonalList} />
-                <Route exact path="/Waste" component={Waste} />
-              </div>
-=======
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/lists" component={MyLists} />
               <Route exact path="/pantry" component={Pantry} />
               <Route exact path="/recipes" component={Recipes} />
               <Route exact path="/waste" component={Waste} />
->>>>>>> bryan
             </main>
 
             <footer>
