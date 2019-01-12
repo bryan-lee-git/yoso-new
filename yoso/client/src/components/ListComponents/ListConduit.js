dc016087bc1dffdf83a3dee390f47febd67cb70d
Jim Schlader <jamesschlader@yahoo.com>
2019-01-12 09:15:37 -0700

    best guess for mvp

--- yoso/client/src/components/ListComponents/ListConduit.js
+++ yoso/client/src/components/ListComponents/ListConduit.js
@@ -11,13 +11,13 @@ import ListAPI from "../../utilities/ListAPI";
 export default class ListConduit extends Component {
   constructor(props) {
     super(props);
+
     this.state = { lists: ["this list"], view: 0 };
   }
 
   componentDidMount() {
-    console.log(`inside the conduit, here's props: `, this.props.state.id);
-    console.log(`inside the conduit, here's props: `, this.props.rerender);
     console.log(`inside the conduit, here's props: `, this.props);
+
     ListAPI.getLists(this.props.state.id).then(res => {
       console.log(
         `From getlists at listconduit, here's the user's lists: `,
@@ -44,44 +44,18 @@ export default class ListConduit extends Component {
 
   render() {
     const { view } = this.state;
+    console.log(`inside listconduit, we fired off render! Here's ${view}`);
 
-    let listView;
-    if (view === 0) {
-      listView = (
-        <ListSwitch stuff={this.props.state} handleSwitch={this.handleSwitch} />
-      );
-    } else if (view === 1) {
-      listView = (
-        <NewList
-          handleSwitch={this.handleSwitch}
-          renderState={this.props.renderState}
-        />
-      );
-    } else if (view === 2) {
-      listView = (
-        <UserLists
-          lists={this.state.lists}
-          handleSwitch={this.handleSwitch}
-          renderState={this.props.renderState}
-        />
-      );
-    } else if (view === 3) {
-      listView = (
-        <ShopList
-          handleSwitch={this.handleSwitch}
-          renderState={this.props.renderState}
-        />
-      );
-    }
     return (
       <Container>
-        <LogoRow goto="/lists" />
         <Row className="btn-row">
-          <Col s={12}>
-            <br />
-            <br />
-          </Col>
-          {listView}
+          {view === 0 ? (
+            <ListSwitch stuff={this.state} handleSwitch={this.handleSwitch} />
+          ) : view == 1 ? (
+            <NewList view={view} handleSwitch={this.handleSwitch} />
+          ) : (
+            <UserLists view={view} handleSwitch={this.handleSwitch} />
+          )}
         </Row>
       </Container>
     );
--- yoso/client/src/components/ListComponents/ListSwitch.js
+++ yoso/client/src/components/ListComponents/ListSwitch.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NoListWarning from "./NoListWarning";
 import SwitchButton from "./SwitchButton";
+import LogoRow from "../LogoRow";
 
 export default props => {
   const { handleSwitch, handleRerender } = props;
   console.log(`inside listswitch, here's props: `, props);
   return (
     <React.Fragment>
+      <LogoRow goto="/home" />
       {props.stuff.lists.length === 0 ? (
         <div>
           <NoListWarning />
@@ -22,18 +24,13 @@ export default props => {
           <SwitchButton
             view="new-list"
             text="NEW LIST"
+            value={1}
             onClick={handleSwitch}
           />
           <SwitchButton
             view="my-lists"
             value={2}
             text="MY LISTS"
-            onClick={handleSwitch}
-          />
-          <SwitchButton
-            view="my-lists"
-            value={3}
-            text="GO SHOPPING"
             onClick={handleSwitch}
           />
         </div>
