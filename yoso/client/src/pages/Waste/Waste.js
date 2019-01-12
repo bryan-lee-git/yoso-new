b84b64409ff62399dee8e3aad63a11b9a0eccbfc
Jim Schlader <jamesschlader@yahoo.com>
2019-01-12 12:17:59 -0700

    moved userContext to the waste page and sent it as props to wasteItems component

--- yoso/client/src/pages/Waste/Waste.js
+++ yoso/client/src/pages/Waste/Waste.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import WasteItem from "../../components/WasteItem";
 import { Container, Row, Col, Icon, Button } from "react-materialize";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
 
 class Waste extends Component {
   render() {
     return (
-      <Container className="center-align">
-        <br />
-        <Row>
-          <Col s={3}>
-            <Link to="/home">
+      <UserContext.Consumer>
+        {userContext => {
+          console.log(`inside waste, here's the context: `, userContext);
+          return (
+            <Container className="center-align">
               <br />
-              <br />
-              <br />
-              <Button>
-                <Icon>arrow_back</Icon>
-              </Button>
-            </Link>
-          </Col>
-          <Col s={6}>
-            <h1 className="white-text fade-in">WASTE LESS!</h1>
-          </Col>
-        </Row>
-      </Container>
+              <Row>
+                <Col s={3}>
+                  <Link to="/home">
+                    <br />
+                    <br />
+                    <br />
+                    <Button>
+                      <Icon>arrow_back</Icon>
+                    </Button>
+                  </Link>
+                </Col>
+                <Col s={6}>
+                  <h1 className="white-text fade-in">WASTE LESS!</h1>
+                </Col>
+              </Row>
+              <WasteItem context={userContext} />
+            </Container>
+          );
+        }}
+      </UserContext.Consumer>
     );
   }
 }
